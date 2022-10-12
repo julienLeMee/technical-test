26920cc00d356b42b7836b89fe93cae302dc4d08
Julien Le Mee <julemee@gmail.com>
2022-10-12 10:06:05 -0400

    refactor css

--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-const popup = document.querySelector('.pop-up-display');
-const close = document.querySelector('.close-button');
-const button = document.querySelector('.form-input-button');
-const container = document.querySelector('.container');
+const $popup = document.querySelector('.pop-up');
+const $close = document.querySelector('.close-button');
+const $button = document.getElementById('submit');
+const $container = document.querySelector('.container');
+const $email = document.getElementById('email');
+const $popupEmail = document.getElementById('popup-email');
 
-button.addEventListener('click', togglePopup)
-close.addEventListener('click', closePopup)
 
-function togglePopup(){
-  popup.classList.toggle('pop-up-display');
-  container.classList.toggle('active');
-}
+// $button.addEventListener('submit', (e) => {
+$button.addEventListener('click', (e) => {
+  e.preventDefault();
+  $popup.classList.add('active');
+  $container.classList.add('active');
+  $popupEmail.textContent = $email.value;
+  $popupEmail.style.fontWeight = 'bold';
 
-function closePopup(){
-  popup.classList.toggle('pop-up-display');
-  container.classList.toggle('active');
-  }
+  document.body.style.pointerEvents = 'none';
+  $popup.style.pointerEvents = 'auto';
+});
+
+$close.addEventListener('click', () => {
+  $popup.classList.remove('active');
+  $container.classList.remove('active');
+
+  document.body.style.pointerEvents = 'auto';
+});
