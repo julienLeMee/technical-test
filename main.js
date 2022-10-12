b8b763b5bc8be545779e5d5d711bfe8111964689
Julien Le Mee <julemee@gmail.com>
2022-10-12 10:44:55 -0400

    fixed submit issue

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const $popup = document.querySelector('.pop-up');
 const $close = document.querySelector('.close-button');
-const $button = document.getElementById('submit');
+const $button = document.getElementById('form-submit');
 const $container = document.querySelector('.container');
 const $email = document.getElementById('email');
 const $popupEmail = document.getElementById('popup-email');
 
 
-// $button.addEventListener('submit', (e) => {
-$button.addEventListener('click', (e) => {
+$button.addEventListener('submit', (e) => {
   e.preventDefault();
   $popup.classList.add('active');
   $container.classList.add('active');
@@ -21,6 +20,5 @@ $button.addEventListener('click', (e) => {
 $close.addEventListener('click', () => {
   $popup.classList.remove('active');
   $container.classList.remove('active');
-
   document.body.style.pointerEvents = 'auto';
 });
